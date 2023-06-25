3c98feb2f531ce6a865ffdefc11fc74812f795f9
navadeep1012 <navadeep1012@gmail.com>
2023-06-25 22:11:48 +0530

    Basic expense tracker project

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+    return (
+      <div>
+        <div>
+          <h3>Expense Items</h3>
+        </div>
+        <div>Food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+      </div>
+    );
+}
+
+export default ExpenseItem;
